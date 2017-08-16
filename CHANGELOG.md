06c90088a1dd6f8e6a93359ec731dff0facfbcfa
Pascal ECHEMANN <developers@flashapi.org>
2017-08-16 11:41:18 +0200

    New Release Update
    1.1.3 release update.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,16 @@
 # JEC JUTA Project: Update Release Notes
 
+<a name="jec-juta-1.1.3"></a>
+## **1.1.3** (2017-08-16)
+
+### Bug Fixes
+
+### Features
+
+- **build**: adding build script to npm install process for GlassCat instal optimisation
+- **index.ts**: refactoring index.ts file for better types generation
+- Dependencies upgrade
+
 <a name="jec-juta-1.1.2"></a>
 ## **1.1.2** (2017-07-17)
 
---
+++ src/com/jec/jec-juta-index.ts
@@ -0,0 +1,44 @@
+/*!
+ * JEC JUTA Node Module
+ * Copyright(c) 2017 Pascal ECHEMANN
+ * Apache 2.0 Licensed
+ * This is a part of the JEC Projects: <https://github.com/pechemann/JEC>
+ */
+
+"use strict";
+
+/*!
+ * Module dependencies.
+ * Please maintain package and alphabetical order!
+ */
+
+//--> com/jec/juta/annotations/core
+export {AnnotatedMethodParams} from "./juta/annotations/core/AnnotatedMethodParams";
+export {TestParams} from "./juta/annotations/core/TestParams";
+export {TestSuiteParams} from "./juta/annotations/core/TestSuiteParams";
+//--> com/jec/juta/annotations
+export {After} from "./juta/annotations/After";
+export {Async} from "./juta/annotations/Async";
+export {AfterClass} from "./juta/annotations/AfterClass";
+export {AfterAll} from "./juta/annotations/AfterAll";
+//export {AfterClass} from "./juta/annotations/AfterClass";
+export {Before} from "./juta/annotations/Before";
+export {BeforeAll} from "./juta/annotations/BeforeAll";
+export {BeforeClass} from "./juta/annotations/BeforeClass";
+export {Test} from "./juta/annotations/Test";
+export {TestSuite} from "./juta/annotations/TestSuite";
+//--> com/jec/juta/exceptions
+export {TestSuiteError} from "./juta/exceptions/TestSuiteError";
+//--> com/jec/juta/jcad
+export {JutaConnectorRefs} from "./juta/jcad/JutaConnectorRefs";
+//--> com/jec/juta/runner
+export {AnnotatedMethod} from "./juta/runner/AnnotatedMethod";
+export {RunableTestSuite} from "./juta/runner/RunableTestSuite";
+export {TestableMethod} from "./juta/runner/TestableMethod";
+export {TestMethod} from "./juta/runner/TestMethod";
+export {TestRunner} from "./juta/runner/TestRunner";
+export {TestStats} from "./juta/runner/TestStats";
+//--> com/jec/juta/utils
+export {InstantiationPolicy} from "./juta/utils/InstantiationPolicy";
+export {AnnotatedMethodType} from "./juta/utils/AnnotatedMethodType";
+export {TestSorters} from "./juta/utils/TestSorters";
